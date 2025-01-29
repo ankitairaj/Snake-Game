0d084b80f9ca125322a1f97a8f3f886dd0a06389
anki2806 <ankit80519@gmail.com>
2025-01-29 13:33:25 +0530

    added scoreboard

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Script is working fine");
+document.addEventListener('DOMContentLoaded', () => {
+    const gameArena = document.getElementById("game-arena");
+    const arenaSize = 600;
+    const cellSize = 20;
+    let score = 0;
+    let gameStarted = false;
+    let food = {x : 300, y : 200};
+    let snake = [{x:160, y:200},{x:140, y:200},{x:120, y:200}];
+
+    let dx = cellSize;
+    let dy = 0;
+
+    function startGame() {
+        const scoreBoard = document.createElement('div');
+        scoreBoard.id = 'score-board';
+        // scoreBoard.textContent = '10';
+        document.body.insertBefore(scoreBoard, gameArena);
+        
+
+        const startButton = document.createElement('button');
+        startButton.textContent= 'Start Game';
+        startButton.classList.add('start-button');
+        document.body.appendChild(startButton);
+    }
+
+    startGame();
+
+
+
+});
